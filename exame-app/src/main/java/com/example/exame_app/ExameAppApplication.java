4dcae76a0fc4c50b40fbb725bcea924d253bb8d5
Eliziajs <eliziajs@gmail.com>
2024-09-22 17:43:51 -0300

    Criação do Application.properties/ Creating Application.properties

---
+++ exame-app/src/main/java/com/example/exame_app/ExameAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.exame_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExameAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExameAppApplication.class, args);
+		System.out.println("amém");
+	}
+
+}
---
+++ exame-app/src/test/java/com/example/exame_app/ExameAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exame_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExameAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
